8b40dc81c906f981bf20183da8471174080e7350
s3miOtics <colby.kauk@gmail.com>
2023-03-31 13:09:53 -0700

    config store

---
+++ client/src/state/index.js
@@ -0,0 +1,20 @@
+import { dark } from "@mui/material/styles/createPalette"
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    mode: "dark",
+};
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState, 
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+    },
+});
+
+export const { setMode } = globalSlice.actions;
+
+export default globalSlice.reducer;
