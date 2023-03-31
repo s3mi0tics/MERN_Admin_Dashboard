a6eaf8f126b4a8200d7f021c80059845d4468341
s3miOtics <colby.kauk@gmail.com>
2023-03-31 10:04:49 -0700

    setup mongoose

--- server/index.js
+++ server/index.js
@@ -25,3 +25,12 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
