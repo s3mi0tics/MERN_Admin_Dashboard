99919a7df9fb78194d41f48ee6464dc197992efb
s3miOtics <colby.kauk@gmail.com>
2023-03-31 13:09:19 -0700

    setup material ui in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-
+import { CssBaseline, ThemePrivider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-  
+      <ThemePrivider theme={theme}>
+        <CssBaseline />
+      </ThemePrivider>
     </div>
   );
 }
