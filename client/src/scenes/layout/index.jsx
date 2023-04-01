df42f897b98784bc5f14a38de8bed20595dc42ef
s3miOtics <colby.kauk@gmail.com>
2023-04-01 14:43:22 -0700

    add collapsable sidebar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -2,12 +2,26 @@ import React, {useState} from 'react'
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import NavBar from "components/NavBar"
+import NavBar from "components/Navbar"
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
+
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return <Box width="100%" height="100%">
     <Box>
-        <NavBar />
+        <Sidebar
+          isNonMobile={isNonMobile}
+          drawerWidth="250px"
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <NavBar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
     </Box>
   </Box>
