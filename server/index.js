c5c18e35e5e09a1dc3bdda2ddc44111f2454604e
s3miOtics <colby.kauk@gmail.com>
2023-04-03 00:27:36 -0700

    add data to database

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js"
 import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
+//data imports
+import User from "./models/User.js"
+import { dataUser } from "./data/index.js"
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -33,4 +37,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    
+    /*  ONLY ADD DATA ONE TIME */
+    // User.insertMany(dataUser);
 }).catch((error) => console.log(`${error} did not connect`))
