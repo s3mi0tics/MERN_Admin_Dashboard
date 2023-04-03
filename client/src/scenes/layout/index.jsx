c65e87205cc435bdf717e55afe7c6b39c70fdd03
s3miOtics <colby.kauk@gmail.com>
2023-04-03 00:28:37 -0700

    add userQuery to index.js

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,30 +1,38 @@
-import React, {useState} from 'react'
-import { Box, useMediaQuery } from "@mui/material";
-import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import NavBar from "components/Navbar"
-import Sidebar from "components/Sidebar";
+import React, { useState } from 'react'
+import { Box, useMediaQuery } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import NavBar from 'components/Navbar'
+import Sidebar from 'components/Sidebar'
+import { useGetUserQuery } from 'state/api'
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery('(min-width: 600px)')
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const userId = useSelector((state) => state.global.userId)
+  const { data } = useGetUserQuery(userId)
 
-  const isNonMobile = useMediaQuery("(min-width: 600px)");
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  return <Box width="100%" height="100%">
-    <Box>
+  return (
+    <Box display={isNonMobile ? 'flex' : 'block'} width="100%" height="100%">
+      <Box>
         <Sidebar
+          user={data || {}}
           isNonMobile={isNonMobile}
           drawerWidth="250px"
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
-        <NavBar
-          isSidebarOpen={isSidebarOpen}
-          setIsSidebarOpen={setIsSidebarOpen}
-        />
-        <Outlet />
+        <Box flexGrow={1}>
+          <NavBar
+            user={data || {}}
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+          />
+          <Outlet />
+        </Box>
+      </Box>
     </Box>
-  </Box>
+  )
 }
 
 export default Layout
