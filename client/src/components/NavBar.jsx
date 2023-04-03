505d247685aad7a8a86a39065f45e891e08789b4
s3miOtics <colby.kauk@gmail.com>
2023-04-03 00:29:53 -0700

    build sidebar and navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -11,58 +11,118 @@ import FlexBetween from 'components/FlexBetween'
 import { useDispatch } from 'react-redux'
 import { setMode } from 'state'
 import profileImage from 'assets/me2.png'
-import { AppBar, Toolbar, useTheme, InputBase, IconButton } from '@mui/material'
+import {
+  AppBar,
+  Toolbar,
+  useTheme,
+  InputBase,
+  IconButton,
+  MenuItem,
+  Menu,
+  Typography,
+  Button,
+  Box,
+} from '@mui/material'
 
-const NavBar = ({
-    isSideBarOpen,
-    setIsSidebarOpen
-}) => {
+const NavBar = ({ user, isSideBarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch()
   const theme = useTheme()
 
-  return(
-      <AppBar
-        sx={{
-          position: 'static',
-          background: 'none',
-          boxShadow: 'none',
-        }}
-      >
-        <Toolbar sx={{ justifyContent: "space-between"}}>
-            {/* LEFT SIDE */}
-            <FlexBetween>
-                <IconButton onClick={() => setIsSidebarOpen(!isSideBarOpen)}>
-                    <MenuIcon />
-                </IconButton>
-                <FlexBetween
-                    backgroundColor={theme.palette.background.alt}
-                    borderRadius = "9px"
-                    gap="3rem"
-                    p="0.1rem 1.5rem"
+  const [anchorEl, setAnchorEl] = useState(null)
+  const isOpen = Boolean(anchorEl)
+  const handleClick = (event) => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null)
+
+  return (
+    <AppBar
+      sx={{
+        position: 'static',
+        background: 'none',
+        boxShadow: 'none',
+      }}
+    >
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        {/* LEFT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => setIsSidebarOpen(!isSideBarOpen)}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkModeOutlined sx={{ fontSize: '25px' }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: '25px' }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: '25px' }} />
+          </IconButton>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-betweeen',
+                alignItems: 'center',
+                textTransform: 'none',
+                gap: '1rem',
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: 'cover' }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
                 >
-                    <InputBase placeholder="Search..." />
-                    <IconButton>
-                        <Search />
-                    </IconButton>
-                </FlexBetween>
-            </FlexBetween>
-
-            {/* RIGHT SIDE */}
-            <FlexBetween gap="1.5rem">
-                <IconButton onClick={() => dispatch(setMode())}>
-                    {theme.palette.mode === 'dark' ? (
-                        <DarkModeOutlined sx={{ fontSize: '25px'}} />
-                    ):(
-                        <LightModeOutlined sx={{ fontSize: "25px"}} />
-                    )}
-                </IconButton>
-                <IconButton>
-                    <SettingsOutlined sx={{ fontSize: "25px"}} />
-                </IconButton>
-            </FlexBetween>
-        </Toolbar>
-      </AppBar>
-
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: '25px' }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            >
+              <MenuItem onCLick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
   )
 }
 
