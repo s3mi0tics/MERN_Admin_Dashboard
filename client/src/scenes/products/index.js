a26c7b48ea3abe228bf5dd325553b74def3ec726
s3miOtics <colby.kauk@gmail.com>
2023-04-06 18:37:02 -0700

    build products index.js

---
+++ client/src/scenes/products/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { useGetProductsQuery } from "state/api";
+import Header from "components/Header"
+
+const Products = () => {
+  return <Box>
+    <Header title="PRODUCTS" subtitle="See your list of products" />
+  </Box>;
+};
+
+export default Products;
