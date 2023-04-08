446021a257a86ad7e7eb393b83a9120856bd83b4
s3miOtics <colby.kauk@gmail.com>
2023-04-07 18:13:39 -0700

    import transactions model and upload to database

--- server/index.js
+++ server/index.js
@@ -10,11 +10,13 @@ import generalRoutes from "./routes/general.js"
 import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
+
 //data imports
 import User from "./models/User.js"
 import Product from "./models/Products.js"
 import ProductStat from './models/ProductStat.js';
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js"
+import Transaction from './models/Transaction.js'
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
 
 /* CONFIGURATION */
 dotenv.config();
@@ -43,5 +45,6 @@ mongoose.connect(process.env.MONGO_URL, {
     /*  ONLY ADD DATA ONE TIME */
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
 }).catch((error) => console.log(`${error} did not connect`))
