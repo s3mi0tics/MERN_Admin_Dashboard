ba06095529ac6e00ba2a159407eb23b633973325
s3miOtics <colby.kauk@gmail.com>
2023-04-07 18:12:01 -0700

    add getTransactions to server/controller

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Products.js"
 import ProductStat from "../models/ProductStat.js"
 import User from "../models/User.js"
+import Transaction from "../models/Transaction.js"
 
 
 export const getProducts = async (req, res) => {
@@ -29,6 +30,44 @@ export const getCustomers = async (req, res) => {
     try {
         const customers = await User.find().select("-password")
         res.status(200).json(customers)
+    } catch(error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const getTransactions = async (req, res) => {
+    try {
+        // Sort should look like this: { "field": "userId", "sort": "desc" }
+        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query
+        
+        // Formatted sort should look like { userId: -1}
+        const gererateSort = () => {
+            const sortParsed = JSON.parse(sort)
+            const sortFormatted = {
+                [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+            }
+            return sortFormatted;
+        }
+        const sortFormatted = Boolean(sort) ? generateSort() : {}
+        
+        const transactions = await Transaction.find({
+            $or: [
+                { cost: { $regex: new RegExp(search, "i")}},
+                { userId: { $regex: new RegExp(search, "i")}}
+            ],
+        })
+            .sort(sortFormatted)
+            .skip(page * pageSize)
+            .limit(pageSize)
+
+        const total = await Transaction.countDocuments({
+            name: { $regex: search, $options: "i" }
+        })
+
+        res.status(404).json({
+            transactions,
+            total,
+        })
     } catch(error) {
         res.status(404).json({ message: error.message })
     }
