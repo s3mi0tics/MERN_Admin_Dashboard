9e4627089b0f3d203e7f35d9d36d8b0d5833b8bd
s3miOtics <colby.kauk@gmail.com>
2023-04-14 10:41:13 -0700

    add OverallStat data to the mongoDB database

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ import User from "./models/User.js"
 import Product from "./models/Products.js"
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js'
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
+import OveralStat from './models/OverallStat.js'
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
+
 
 /* CONFIGURATION */
 dotenv.config();
@@ -43,6 +45,7 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
     
     /*  ONLY ADD DATA ONE TIME */
+    // OverallStat.insertMany(dataOverallStat)
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
