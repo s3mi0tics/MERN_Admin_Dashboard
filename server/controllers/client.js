0254d8c43e4889ee4298e6ac402f391a76b25c03
s3miOtics <colby.kauk@gmail.com>
2023-04-14 10:45:11 -0700

    add HTTP GET method handler for geography

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Products.js"
 import ProductStat from "../models/ProductStat.js"
 import User from "../models/User.js"
 import Transaction from "../models/Transaction.js"
+import getCountyIso3 from "country-iso-2-to-3"
 
 
 export const getProducts = async (req, res) => {
@@ -68,6 +69,31 @@ export const getTransactions = async (req, res) => {
             transactions,
             total,
         })
+    } catch(error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const getGeography = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        const mappedLocations = users.reduce((acc, { country }) => {
+            const countryISO3 = getCountyIso3(country)
+            if(!acc[countryISO3]) {
+                acc[countryISO3] = 0;
+            }
+            acc[countryISO3]++;
+            return acc;
+        }, {})
+
+        const formattedLocations = Object.entries(mappedLocations).map(
+            ([country, count]) => {
+                return { id: country, value: count}
+            }
+        )
+        
+        res.status(200).json(formattedLocations)
     } catch(error) {
         res.status(404).json({ message: error.message })
     }
