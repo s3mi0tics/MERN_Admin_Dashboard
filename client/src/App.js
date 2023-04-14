9e1691f08b600be03ed06a96d6f4c5d0134d9f74
s3miOtics <colby.kauk@gmail.com>
2023-04-14 10:28:24 -0700

    Add overview and geography routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Dashboard from "scenes/dashboard";
 import Products from "scenes/products";
 import Customers from "scenes/customers"
 import Transactions from "scenes/transactions"
+import Geography from "scenes/geography"
+import Overview from "scenes/overview"
 
 
 function App() {
@@ -26,6 +28,8 @@ function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
             </Route>
           </Routes>
         </ThemeProvider>
