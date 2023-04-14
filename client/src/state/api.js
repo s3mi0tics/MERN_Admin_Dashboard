01a75cd8027b7dde6651650b18fe0ca97322865f
s3miOtics <colby.kauk@gmail.com>
2023-04-14 10:30:16 -0700

    set up endpoints for geography and sales in api.js

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
-    tagTypes: ["User", "Products", "Customers", "Transactions"],
+    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales"],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -24,8 +24,23 @@ export const api = createApi({
                 params: { page, pageSize, sort, search }
             }), 
             providesTags: ["Transactions"]
+        }),
+        getGeography: build.query({
+            query: () => "client/geography",
+            providesTags: ["Geography"],
+        }),
+        getSales: build.query({
+            query: () => "sales/sales",
+            providesTags: ["Sales"]
         })
     })
 })
 
-export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery } = api
+export const { 
+    useGetUserQuery,
+    useGetProductsQuery, 
+    useGetCustomersQuery, 
+    useGetTransactionsQuery, 
+    useGetGeographyQuery, 
+    useGetSalesQuery,
+} = api
