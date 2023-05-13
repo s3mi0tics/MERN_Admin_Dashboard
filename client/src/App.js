870e949ea892c810e6d93732fd63d3111c5cf498
s3miOtics <colby.kauk@gmail.com>
2023-05-12 18:59:58 -0700

    add breakdown to browser router

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Transactions from "scenes/transactions"
 import Geography from "scenes/geography"
 import Overview from "scenes/overview"
 import Daily from "scenes/daily"
+import Monthly from "scenes/monthly"
+import Breakdown from "scenes/breakdown"
 
 
 function App() {
@@ -32,6 +34,8 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
