8463e084ccea7b86c0dca0154428e21bc13ba5a3
s3miOtics <colby.kauk@gmail.com>
2024-03-02 08:05:04 -0800

    add error messages to api queries

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -18,43 +18,71 @@ export const api = createApi({
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
-            providesTags: ["User"]
+            providesTags: ["User"],
+            onError: (error) => {
+                console.error('Error fetching User:', error);
+            },
         }),
         getProducts: build.query({
             query: () => "client/products",
             providesTags: ["Products"],
+            onError: (error) => {
+                console.error('Error fetching products:', error);
+            },
         }),
         getCustomers: build.query({
             query: () => "client/customers",
             providesTags: ["Customers"],
+            onError: (error) => {
+                console.error('Error fetching customers:', error);
+            },
         }),
         getTransactions: build.query({
             query: ({ page, pageSize, sort, search }) => ({
                 url: "client/transactions",
                 method: "GET",
-                params: { page, pageSize, sort, search }
+                params: { page, pageSize, sort, search },
+                
             }), 
-            providesTags: ["Transactions"]
+            providesTags: ["Transactions"], 
+            onError: (error) => {
+                    console.error('Error fetching transactions:', error);
+                },
         }),
         getGeography: build.query({
             query: () => "client/geography",
             providesTags: ["Geography"],
+            onError: (error) => {
+                console.error('Error fetching geography:', error);
+            },
         }),
         getSales: build.query({
             query: () => "sales/sales",
-            providesTags: ["Sales"]
+            providesTags: ["Sales"],
+            onError: (error) => {
+                console.error('Error fetching sales:', error);
+            },
         }),
         getAdmins: build.query({
             query: () => "management/admins",
-            providesTags: ["Admins"]
+            providesTags: ["Admins"],
+            onError: (error) => {
+                console.error('Error fetching Admins:', error);
+            },
         }),
         getUserPerformance: build.query({
             query: (id) => `management/performance/${id}`,
-            providesTags: ["Performance"]
+            providesTags: ["Performance"],
+            onError: (error) => {
+                console.error('Error fetching user performance:', error);
+            },
         }),
         getDashboard: build.query ({
             query: () => "general/dashboard",
-            providesTags: ["Dashboard"]
+            providesTags: ["Dashboard"],
+            onError: (error) => {
+                console.error('Error fetching dashboard:', error);
+            },
         })
     })
 })
